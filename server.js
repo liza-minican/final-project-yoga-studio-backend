e4e20e332d68931bca26091e03789bae17d5ddb3
Liza Minicangit config --global user.email liza.minican@gmail.com <liza.minican@gmail.com>
2021-02-16 21:59:49 +0100

    deployment

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // list endpoints in the '/' route
-const listEndpoints = require("express-list-endpoints");
+// const listEndpoints = require("express-list-endpoints");
 
 // error messages
 const ERR_CANNOT_SAVE_TO_DATABASE = "Could not save video to the Database";
@@ -47,15 +47,9 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
-  //reference to the videos, storing them in the array
+  //reference to the videos, saving them in the array
   selectedVideos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
-
-  // selectedVideos: {
-  //   type: Array,
-  // },
 });
-
-//week 18th and date 16th watch
 
 const videoSchema = new mongoose.Schema(
   {
@@ -80,10 +74,6 @@ const videoSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
-    //RATING AND LIKES CAN BE ADDED LATER
-    // rating: { //   type: Number,
-    //   enum: [1,2,3,4,5]
-    // },
 
     likes: {
       type: Number,
@@ -91,12 +81,6 @@ const videoSchema = new mongoose.Schema(
       min: 0,
       max: 1,
     },
-
-    //reference to the user
-    //   user: {
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref: "User",
-    //   },
   },
   { timestamps: true }
 );
@@ -192,29 +176,9 @@ app.post("/sessions", async (req, res) => {
   }
 });
 
-//Logout
-// app.post("/users/logout", authenticateUser);
-// app.post("users/logout", async (req, res) => {
-//   try {
-//     const { userId } = req.body;
-//     const updatedUser = await User.findOneAndUpdate(
-//       //need to see why the user ID stays
-//       { _id: userId },
-//       { accessToken: null },
-//       { new: true, useFindAndModify: false }
-//     );
-//     res.status(200).json({
-//       userId: updatedUser._id,
-//       accessToken: updatedUser.accessToken,
-//     });
-//   } catch (err) {
-//     res.status(400).json({ error: err, message: "Could not log out" });
-//   }
-// });
-
 // HERE ARE THE ENDPOINTS TO WORK WITH VIDEO COLLECTION
 
-//get the collection of all videos available to everyone { WORKING }
+//get the collection of all videos available to everyone
 app.get("/videos", async (req, res) => {
   const videos = await Video.find()
     .sort({ createdAt: "desc" })
@@ -223,9 +187,7 @@ app.get("/videos", async (req, res) => {
   res.json(videos);
 });
 
-//top 10 videos
-
-//Route to find one specific video by id { WORKING }
+//Route to find one specific video by id
 app.get("/videos/:id", async (req, res) => {
   const videoById = await Video.findOne({ _id: req.params.id });
   if (videoById) {
@@ -235,9 +197,10 @@ app.get("/videos/:id", async (req, res) => {
   }
 });
 
-//filter videos by length
+//filter videos by length, endpoint that can be developed in the future
+//at the moment frontend is responsible for filtering
 
-//filter short videos - 15, 30, 60
+//filter videos - 15, 20, 30
 app.get("/videos/length/30", async (req, res) => {
   const videosByLength = await Video.find({ length: 30 });
   if (videosByLength) {
@@ -262,55 +225,12 @@ app.get("/videos/length/20", async (req, res) => {
     res.status(404).json({ error: "No such video found" });
   }
 });
-// app.get("/videos/duration/:length", async (req, res) => {
-//   const videoByDuration = req.params.length;
-//   const video = await Video.findAll({
-//     // //length: { $regex:("/[[0-9]+]/") }
-//     length: 20,
-//   });
-//   if (video.length > 0) {
-//     res.json(video);
-//   } else {
-//     res.status(404).json({ error: "No such video found" });
-//   }
-// });
+
 // PUT: endpoint to add favoritevideo for a logged-in user:
 // UPDATES the user and adds the favorite video to the favorite videos-array for that user.
 
 //<----------SHOW FAVORITE VIDEOS, ADD TO FAVORITE AND DELETE FROM FAVORITE---------->
 //--ADD--
-// app.put("/users/:userId/favorites/:videoId", authenticateUser);
-// app.put("/users/:userId/favorites/:videoId", async (req, res) => {
-//   const { userId, videoId } = req.params;
-//   try {
-//     const selectedVideo = await Video.findById(videoId); // Find the video the user wants to add.
-//     const selectedVideos = await req.user.selectedVideos; // array of selected videos in user model
-//     //const getCurrentSelectedVideos = await Video.find({
-//     //  _id: selectedVideos,
-//    //});
-//     //console.log("currentselected", getCurrentSelectedVideos);
-//     //console.log("selectedVideo", selectedVideo);
-//     console.log("selectedVideos", selectedVideos);
-//     //if (selectedVideos.some((video) => video._id === selectedVideo._id)) {
-//     //  alert("Video already existst!");
-//     //} else {
-//      // alert("Object not found.");
-//       await User.updateOne(
-//         { _id: userId },
-//         { $push: { selectedVideos: selectedVideo } } //push the selected video into the favorite videos array
-//       );
-//     }
-//     console.log("");
-//     res.status(200).json(selectedVideo);
-//   } catch (err) {
-//     res.status(404).json({
-//       message: "Could not add video.",
-//       errors: { message: err.message, error: err },
-//     });
-//   }
-// });
-
-//----old version without filtering duplicates
 app.put("/users/:userId/favorites/:videoId", authenticateUser);
 app.put("/users/:userId/favorites/:videoId", async (req, res) => {
   const { userId, videoId } = req.params;
@@ -321,7 +241,6 @@ app.put("/users/:userId/favorites/:videoId", async (req, res) => {
       { _id: userId },
       { $push: { selectedVideos: selectedVideo } } //push the selected video into the favorite videos array
     );
-    //console.log("")
     res.status(200).json(selectedVideo);
   } catch (err) {
     res.status(404).json({
@@ -330,6 +249,7 @@ app.put("/users/:userId/favorites/:videoId", async (req, res) => {
     });
   }
 });
+
 //--DELETE--
 app.delete("/users/:userId/favorites/:videoId", authenticateUser);
 app.delete("/users/:userId/favorites/:videoId", async (req, res) => {
@@ -341,8 +261,6 @@ app.delete("/users/:userId/favorites/:videoId", async (req, res) => {
       { _id: userId },
       { $pull: { selectedVideos: { $in: [selectedVideo] } } } //delete the selected video from the favorite videos array
     );
-    //$pull: { fruits: { $in: [ "apples", "oranges" ] }
-    //console.log("")
     res.status(200).json(selectedVideo);
   } catch (err) {
     console.log(err);
@@ -353,6 +271,7 @@ app.delete("/users/:userId/favorites/:videoId", async (req, res) => {
   }
 });
 //--SHOW FAVORITE VIDEOS--
+
 app.get("/users/:userId/favorites", authenticateUser);
 app.get("/users/:userId/favorites", async (req, res) => {
   try {
@@ -373,7 +292,7 @@ app.get("/users/:userId/favorites", async (req, res) => {
   }
 });
 
-//i need to limit likes on and off for ine particulat user to be able to filter videos by total amount of likes
+//likes are not used in the project at the moment, but can be used in the future
 app.post("/videos/:id/like", async (req, res) => {
   try {
     const { id } = req.params;
@@ -386,7 +305,7 @@ app.post("/videos/:id/like", async (req, res) => {
   }
 });
 
-//post a video, function available only for admins { WORKING }
+//post a video, function available only for admins
 app.post("/videos", async (req, res) => {
   //Try catch
   try {
@@ -422,37 +341,6 @@ app.delete("/videos/:id", async (req, res) => {
     res.status(400).json({ success: false });
   }
 });
-
-// updating a video endpoint
-// app.patch("/videos/:id", async (req, res) => {
-//   try {
-//     //try to delete and send a successful response
-//     const video = await Video.findByID({ _id: id });
-//     const {
-//       videoName = video.videoName,
-//       videoUrl = video.videoUrl,
-//       description = video.description,
-//       category = video.category,
-//       length = video.length,
-//     } = req.body;
-//     const updateVideo = await Video.findByIdAndUpdate(
-//       { _id: id },
-//       { videoName, videoUrl, description, category, length },
-//       { runValidators: true }
-//     );
-//     return res.status(202).json(updatedVideo);
-//   } catch (error) {
-//     try {
-//       await Video.findById({ _id: id }); // id is matching, the model validation is off
-//       return res
-//         .status(400)
-//         .json({ message: "Could not update video", error: err.message });
-//     } catch (err) {
-//       // id is not matching, the validation can be either right or wrong
-//       return res.status(404).json({ message: ERR_CANNOT_FIND_VIDEO_BY_ID, id });
-//     }
-//   }
-// });
 
 // Start the server
 app.listen(port, () => {
